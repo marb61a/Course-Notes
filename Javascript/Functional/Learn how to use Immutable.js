8f6c61c77d604bc8d1ee90a93bd5e22dadaf0b93
marb61a <marb61a@gmail.com>
2017-08-04 17:19:14 +0100

    initial commit

---
+++ Javascript/Functional/Learn how to use Immutable.js
@@ -0,0 +1,36 @@
+                    Learn how to use Immutable.js
+                    Course Notes
+       
+       
+1 - Manage Application State With Immutable JS
+
+2 - Creating An Immutable Object Graph With Immutable.js
+
+3 - Modifying An Immutable.js Map
+
+4 - Querying An Immutable.js Map
+
+5 - Iterating Over An Immutable.js Map
+
+6 - Working With Subsets Of An Immutable.js Map
+
+7 - Equality Checking With Is And More
+
+8 - Using Fromjs To Convert Plain Javascript Objects Into Immutable Data
+
+9 - Differences Between The Immutable.js Map And List
+
+10 - Exploring Sequences And Ranges In Immutable.js
+
+11 - Converting Immutable.js Structures To Javascript And Other Immutable Types
+
+12 - Simple Undo-Redo With Immutable.js Data Structures
+
+13 - Maintaining Order In Immutable.js Objects
+
+14 - Transforming Immutable Data With Reduce
+
+15 - Lightning Fast Immutable.js Equality Checks With Hash Codes
+
+16 - Immutable Record As Data Models
+
