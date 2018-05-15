199d1c064c178ba53bc8f5f4cfdc5a53d819fde0
Martin Byrne <marb61a@gmail.com>
2018-05-15 18:15:51 +0100

    Create Offline-First Progressive Web Apps (PWA) in Vue.js

---
+++ Web Development/Vue/Offline-First Progressive Web Apps (PWA) in Vue.js
@@ -0,0 +1,36 @@
+                    Offline-First Progressive Web Apps (PWA) in Vue.js
+                    Course Notes
+                    
+                    
+1 -  Intro to Offline-First Progressive Web Apps in Vue.js
+
+2 - Add a Web App Manifest to Create a Progressive Web Application
+
+3 - Add Meta Tags for Other Browsers on a PWA
+
+4 - Cache Static Assets with Service Workers using sw-precache
+
+5 - Integrate Service Worker Generation into Webpack's Build with SW-Precache
+
+6 - Register a Service Worker in Production
+
+7 - Hot Reload Static Files by Clearing Service Worker Cache while Developing
+
+8 - Use a no-op Service Worker in Development
+
+9 - Show Notifications when a Service Worker is Installed or Updated
+
+10 - Cache External Resources during Runtime with sw-precache
+
+11 - Cache Data from TCPIP Connections with IndexedDB
+
+12 - Lazy load Images using Intersection Observer API
+
+13 - Check Online Status by using the NavigatorOnLine API
+
+14 - Lazy Load a Route by using the Dynamic Import in Vue.js
+
+15 - Audit your App with Chrome Devtools
+
+16 - Optimize Assets Delivery using preload and prefetch                   
+                    
