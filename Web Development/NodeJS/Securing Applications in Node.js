f239b8071e4cfff4f7ddc5512bbb7a50c98984ff
Martin Byrne <marb61a@gmail.com>
2018-12-19 16:26:33 +0000

    Update and rename Networking and Streams to Securing Applications in Node.js

---
+++ Web Development/NodeJS/Securing Applications in Node.js
@@ -0,0 +1,79 @@
+                    Securing Applications in Node.js 
+                    Course Notes
+
+
+                    Section 1 - INTRODUCTION
+1 - The Course Overview
+A quick intro to the course and the instructor
+There are some prerquisites for the course
+  - A recent version of Node.js should be installed and running
+  - The student should have a good understanding JavaScript
+  - There should be a basic understanding of the request/response cycle in Node
+
+2 - The Importance of Security in Web Applications
+
+3 - Express Is Not Secure by Default
+
+4 - Set Up the Demo Project
+
+
+
+                    Section 2 - CROSS-SITE REQUEST FORGERY
+1 - How to Break the Security of an Application That’s Vulnerable to Cross-Site Request Forgery
+
+2 - Protecting a Site Using CSRF Tokens
+
+3 - Protecting a Site Using Headers
+
+4 - Same-Site Policies on Cookies
+
+
+
+                    Section 3 - SQL INJECTION AND CROSS-SITE SCRIPTING
+1 - SQL Injection in Action
+
+2 - Preventing SQL Injection with Template Literals
+
+3 - Cross-Site Scripting in Action
+
+4 - Preventing Cross-Site Scripting
+
+
+
+                    Section 4 - PASSWORDS AND PASSWORDLESS
+1 - Password User Experience
+
+2 - Securely Storing and Verifying Passwords
+
+3 - Password Reset Flows
+
+4 - Passwordless Authentication
+
+
+
+                    Section 5 - TWO-FACTOR AUTHENTICATION
+1 - Why Is Two-Factor Authentication Important?
+
+2 - SMS-Based Two-Factor Authentication
+
+3 - Google Authenticator
+
+4 - OAUTH
+
+5 - Facebook Login
+
+6 - Google Identity
+
+
+
+                    Section 6 - AUTHORIZATION, DEPLOYMENT AND MANAGING SECRET KEYS
+1 - Authorization in General
+
+2 - Authorization in REST APIs
+
+3 - Authorization in GraphQL
+
+4 - Deployment and Managing Secret Keys
+
+
+
