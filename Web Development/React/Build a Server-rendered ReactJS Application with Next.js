eff0c86eeff29af1f3ddc644e7d230c07d86d040
Martin Byrne <marb61a@gmail.com>
2018-12-23 20:47:17 +0000

    Create Build a Server-rendered ReactJS Application with Next.js

---
+++ Web Development/React/Build a Server-rendered ReactJS Application with Next.js
@@ -0,0 +1,24 @@
+                    Build a Server-rendered ReactJS Application with Next.js
+                    Course Notes
+
+
+1 - Introducing Build a Server-rendered ReactJS application with Next.js
+Intro to both the course and the instructor
+
+2 - Render Text within a Server-side ReactJS App with Next.js
+
+3 - Implement a custom Material-UI theme on the server using a Higher-order component
+
+4 - Render Material-UI Components with Next.js
+
+5 - Populate A Next.js Application with Data using AsyncAwait
+
+6 - Create custom routes within Next.js using ExpressJS
+
+7 - Dynamically Instantiate Routes on Id with Next.js & ExpressJS
+
+8 - Leverage Next.js' getInitialProps lifecycle hook to load individual posts
+
+9 - Style Next.js Application's using CSS-in-JS and styled-jsx
+
+10 - Deploy a Next.js Application to the web with the `now-cli`
