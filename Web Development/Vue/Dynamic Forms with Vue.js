0d6e19a9b09b0d2c18890d9744764600678e5f3a
Martin Byrne <marb61a@gmail.com>
2018-12-28 16:27:47 +0000

    Create Dynamic Forms with Vue.js

---
+++ Web Development/Vue/Dynamic Forms with Vue.js
@@ -0,0 +1,32 @@
+                    Dynamic Forms with Vue.js
+                    Course Notes
+
+
+1 - The Badass Dynamic Form
+The course will cover how to create a multi step form
+There is a GitHub repo to clone
+  - https://github.com/vueschool/dynamic-forms
+    - This has changed from the video url
+  - After cloning cd into the directory and install using either yarn or npm
+
+2 - Communication Between Forms
+
+3 - Disable the Navigation until the Step is Valid
+
+4 - Pass Mamas Data to the Steps
+
+5 - Dynamic Components
+
+6 - Preserve Component State
+
+7 - Control Child Component Through refs
+
+8 - Submit the Mama Form
+
+9 - Add Asynchronous State
+
+10 - Asynchronous Operations Inside Steps
+
+11 - Loading Indicator while the Step Performs Async Operations
+
+12 - Component Communication with Promises Instead of Events
