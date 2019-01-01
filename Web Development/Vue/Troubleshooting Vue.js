6e0321fd5c2e0b6007313e7c95e494f9b8e2e5ee
Martin Byrne <marb61a@gmail.com>
2019-01-01 21:38:07 +0000

    initial commit

---
+++ Web Development/Vue/Troubleshooting Vue.js
@@ -0,0 +1,80 @@
+                    Troubleshooting Vue.js
+                    Course Notes
+
+
+                    Section 1 - TROUBLESHOOTING TECHNIQUES
+1 - The Course Overview
+
+2 - Divide and Conquer
+
+3 - Using the Mathematical Induction Approach
+
+4 - Translating Pseudocode into Code
+
+5 - Simplifying Complex Code into More Readable Code
+
+6 - Using JavaScript Debugger
+
+7 - Unit Testing
+
+
+
+                    Section 2 -SOLVING COMMON ISSUES WITH THE TEMPLATING SYSTEM - THE VIEW
+1 - Improper Use of HTML Based Template Syntax
+
+2 - Missing the Root Element
+
+3 - Incorrect Use of Mustache Tag
+
+4 - Plain Text versus Raw HTML
+
+5 - Common Mistakes Using JavaScript Statements and Flow Controls
+
+6 - Forgetting to Scope CSS Rules
+
+7 - Inline Templates versus X-Templates
+
+
+
+                    Section 3 - TROUBLESHOOTING VUE'S COMPONENTS AND DATA BINDING – THE VIEWMODEL
+1 - Declaring Too Many Global Components
+
+2 - Forgetting to Import/Export a Component
+
+3 - Forgetting to Register a Component
+
+4 - Conflicting Component Names with HTML Elements
+
+5 - The Missing Dot
+
+6 - Component's Data as an Object and Function
+
+7 - Confusing Parentheses with Curly Braces
+
+8 - The v-if and v-show Directives
+
+9 - Common Side Effect Using v-for with Object
+
+10 - Using Mutating versus Non-Mutating Array Methods
+
+
+
+                    Section 4 - UNDERSTANDING ISSUES AND LIMITATIONS WITH FORMS
+1 - Forgetting to Declare Initial Values
+
+2 - The Side Effects of Using Text Interpolation on <textarea>
+
+3 - The Unselected State of the <select> Element
+
+4 - Radio Buttons versus Checkboxes
+
+
+
+                    Section 5 - RESOLVING ISSUES WITH ROUTING AND STATE MANAGEMENT
+1 - Forgetting To Install Vue Router
+
+2 - The <router-view> Tag
+
+3 - Common Mistakes Between this.$route and this.$router
+
+
