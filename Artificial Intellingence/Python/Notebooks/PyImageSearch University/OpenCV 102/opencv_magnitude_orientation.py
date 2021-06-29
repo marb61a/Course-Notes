f6a581bd5aa27d064d631a4d2af614992a0e6bca
Martin Byrne <marb61a@gmail.com>
2021-06-29 20:22:54 +0100

    Update opencv_magnitude_orientation.py

--- Artificial Intellingence/Python/Notebooks/PyImageSearch University/OpenCV 102/opencv_magnitude_orientation.py
+++ Artificial Intellingence/Python/Notebooks/PyImageSearch University/OpenCV 102/opencv_magnitude_orientation.py
@@ -5,6 +5,7 @@
 # Gradient Orientation 
 
 # import the necessary packages
+# Matplotlib will allow for displaying some figures with good formatting
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -21,11 +22,19 @@ image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # compute gradients along the x and y axis, respectively
+# Although using a 64 bit floating point a 32 bit number can be used
+# equally as well
 gX = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
 gY = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
 
 # compute the gradient magnitude and orientation
+# The magnitude is squaring both gX and gY and adding them and then taking
+# the square root
 magnitude = np.sqrt((gX ** 2) + (gY ** 2))
+# Orientation is the arc tangent between gY and gX and then convert to degrees
+# The modulus operation will give either a signed or unsigned gradient
+# This will be seen more when studying image histograms, HOG etc
+# The 180 will clamp degree values between 0 and 180 rather than 360
 orientation = np.arctan2(gY, gX) * (180 / np.pi) % 180
 
 # initialize a figure to display the input grayscle image along with
@@ -33,6 +42,8 @@ orientation = np.arctan2(gY, gX) * (180 / np.pi) % 180
 (fig, axs) = plt.subplots(nrows=1, ncols=3, figsize=(8, 4))
 
 # plot each of the images
+# Uses the Jet colour map, more information can be found at
+# https://stackoverflow.com/questions/7440340/jet-colormap-to-grayscale
 axs[0].imshow(gray, cmap="gray")
 axs[1].imshow(magnitude, cmap="jet")
 axs[2].imshow(orientation, cmap="jet")
