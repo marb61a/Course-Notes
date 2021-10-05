a82df1acac5e4df70d563593b8f5d5d6342afd3f
Martin Byrne <marb61a@gmail.com>
2021-10-05 17:43:10 +0100

    Update minivggnet_cifar10.py

--- Artificial Intellingence/Python/Notebooks/PyImageSearch University/Deep Learning 105/minivggnet/minivggnet_cifar10.py
+++ Artificial Intellingence/Python/Notebooks/PyImageSearch University/Deep Learning 105/minivggnet/minivggnet_cifar10.py
@@ -1,6 +1,27 @@
 # USAGE
 # python minivggnet_cifar10.py --output output/cifar10_minivggnet_with_bn.png
 
+# The text version of the tutorial is available at the following address
+# https://www.pyimagesearch.com/2021/05/22/minivggnet-going-deeper-with-cnns/
+
+# VGGNet was first introduced in a 2014 paper which can be found the following address
+# https://arxiv.org/abs/1409.1556
+# This paper showed that having an architecture with very small (3×3) filters can still
+# be trained to increasingly higher depths (16-19 layers) and obtain state-of-the-art 
+# classification on the challenging ImageNet classification challenge. This is very
+# popular in the deep learning community as it shows what CNN's are capable of achieving
+# Some of the datasets and gpus were not as capable which meant that some things that were
+# being shown by LeNet were not being digested properly. Other factors such as mixed filter
+# sizes being used in network architectures were prevalent, the first layer of a network
+# for example was usuallt between 7x7 and 11x11, it was only the deepest network layers that
+# used a 3x3 size. 
+
+# This is where VGGNet is unique in that it uses 3×3 kernels throughout the entire architecture.
+# The use of these small kernels is arguably what helps VGGNet generalize to classification 
+# problems outside where the network was originally trained. Anytime that a 3x3 network can be
+# seen it is a fair bet that it was inspired by VGGNet, there are 16 and 19 layer variants of
+# VGGNet but that is for a more advanced level.
+
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
 matplotlib.use("Agg")
