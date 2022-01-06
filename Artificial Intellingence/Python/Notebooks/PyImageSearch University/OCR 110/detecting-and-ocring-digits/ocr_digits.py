639644d093413b127b750180aa419f8536ffd7ef
Martin Byrne <marb61a@gmail.com>
2022-01-06 16:40:42 +0000

    Update ocr_digits.py

--- Artificial Intellingence/Python/Notebooks/PyImageSearch University/OCR 110/detecting-and-ocring-digits/ocr_digits.py
+++ Artificial Intellingence/Python/Notebooks/PyImageSearch University/OCR 110/detecting-and-ocring-digits/ocr_digits.py
@@ -2,6 +2,13 @@
 # python ocr_digits.py --image apple_support.png
 # python ocr_digits.py --image apple_support.png --digits 0
 
+# The text version of the tutorial is available at the following address
+# https://www.pyimagesearch.com/2021/08/30/detecting-and-ocring-digits-with-tesseract-and-python/
+
+# The tutorial will cover how to detect and then ocr digits. There are a few different use cases
+# where getting digits from an image is needed eg getting a phone number from a business card
+# or getting figures from an invoice
+
 # import the necessary packages
 import pytesseract
 import argparse
@@ -11,6 +18,9 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image to be OCR'd")
+
+# The digits argument is added as if it is greater than 0 then only digits will be gotten from
+# the image, less than 0 means all parts of the image will be fetched by the ocr engine
 ap.add_argument("-d", "--digits", type=int, default=1,
 	help="whether or not *digits only* OCR will be performed")
 args = vars(ap.parse_args())
